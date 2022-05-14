5168b583243219242bbaa07e9fc431060b3d2ad3
Yahya <47183730+whyrising@users.noreply.github.com>
2022-05-14 16:47:52 +0100

    Add kotest

--- core/src/commonTest/kotlin/NothingTest.kt
+++ core/src/commonTest/kotlin/NothingTest.kt
@@ -1,1 +1,8 @@
-class NothingTest
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.ints.shouldBeExactly
+
+class NothingTest : FreeSpec({
+  "nothing" {
+    1 + 1 shouldBeExactly 2
+  }
+})
